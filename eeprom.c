2807f833c2aac63d9df38b3a678fcb900cbed052
sarvenazrobotics <sarvenazrobotics@gmail.com>
2026-02-28 13:36:46 +0100

    eeprom i2c read dummy write

--- eeprom.c
+++ eeprom.c
@@ -18,10 +18,27 @@ while (1)
 void eeprom_write(char data,unsigned int address)
 {
   i2c_start(); 
+  i2c_write(0x0A); //extracted of datasheet  eeprom address+write
   i2c_write(address>>8);//8bits high
   i2c_write(address);
   i2c_write(data);
   i2c_stop();
 
 };
-char eeprom_read(void) ;
+char eeprom_read(void)
+{     //according to datasheet , it should start with dummy write
+      i2c_start(); 
+      i2c_write(0x0A); //extracted of datasheet eeprom address+write
+      i2c_write(address>>8);//8bits high
+      i2c_write(address);
+      i2c_write(data);
+      i2c_stop();
+
+
+     //read
+    i2c_read(address>>8);
+    i2c_read(address);
+    
+
+
+};
