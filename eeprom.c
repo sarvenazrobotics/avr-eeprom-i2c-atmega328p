36065e3b4d91d3b60b7f6b1255a08c810fdd61ef
sarvenazrobotics <sarvenazrobotics@gmail.com>
2026-02-28 13:12:24 +0100

    I2C init

---
+++ eeprom.c
@@ -0,0 +1,20 @@
+/*
+ * eeprom.c
+ *
+ * Created: 2/28/2026 12:53:46 PM
+ * Author: Sarve
+ */
+
+#include <io.h>
+#include <i2c.h>
+#include <delay.h>
+
+void main(void)
+{
+    i2c_init();
+while (1)
+    {
+    // Please write your application code here
+
+    }
+}
